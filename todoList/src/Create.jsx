ad9c9f79a69a69bc75dce5b85b70ac0017907dab
Misal C. Sathsara <info.misalsathsara@gmail.com>
2024-08-19 21:47:14 +0530

    initial commit

---
+++ todoList/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <div className='create_form'>
+        <input type="text" name="" id="" placeholder='Enter task'/>
+        <button type="button">Add</button>
+    </div>
+  )
+}
+
+export default Create
